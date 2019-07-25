a45fdc85662e5ac7db630791d6727f454fdf864f
Rishi Kaushik <rishi.kaushik15@gmail.com>
2019-07-26 00:07:43 +0530

    changed setup

--- src/test/java/org/ga4gh/RefgetTestSuite/RefgetBaseTest.java
+++ src/test/java/org/ga4gh/RefgetTestSuite/RefgetBaseTest.java
@@ -1,6 +1,5 @@
 package org.ga4gh.RefgetTestSuite;
 
-import org.ga4gh.ComplianceFramework.Server;
 import org.ga4gh.RefgetUtilities.RefgetSession;
 import org.testng.annotations.BeforeTest;
 
@@ -8,7 +7,6 @@ public class RefgetBaseTest {
 
     @BeforeTest
     public void setup(){
-        Server refgetServer = new Server("http://refget.herokuapp.com");
-        RefgetSession.setRefgetServer(refgetServer);
+        RefgetSession.setupEnvironment("http://refget.herokuapp.com");
     }
 }
