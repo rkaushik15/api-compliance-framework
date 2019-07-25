59a076bf28932f467618c30d18a7c87898338e97
Rishi Kaushik <rishi.kaushik15@gmail.com>
2019-07-25 23:58:39 +0530

    added environment setup function in sessions class

--- src/main/java/org/ga4gh/RefgetUtilities/RefgetSession.java
+++ src/main/java/org/ga4gh/RefgetUtilities/RefgetSession.java
@@ -1,17 +1,31 @@
 package org.ga4gh.RefgetUtilities;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.ga4gh.ComplianceFramework.Server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RefgetSession {
 
     private static Server refgetServer;
 
+    public static void setupEnvironment(String server){
+        refgetServer = new Server(server);
+        JsonPath serviceInfoResponse = RefgetUtilities.getServiceInfoResponse(refgetServer).jsonPath();
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("circular_supported", serviceInfoResponse.get("service.circular_supported"));
+        properties.put("algorithms", serviceInfoResponse.get("service.algorithms"));
+        properties.put("subsequence_limit", serviceInfoResponse.get("service.subsequence_limit"));
+        properties.put("supported_api_versions", serviceInfoResponse.get("service.supported_api_versions"));
+
+        refgetServer.setServerProperties(properties);
+    }
 
     public static Server getRefgetServer() {
         return refgetServer;
-    }
-
-    public static void setRefgetServer(Server refgetServer) {
-        RefgetSession.refgetServer = refgetServer;
     }
 }
